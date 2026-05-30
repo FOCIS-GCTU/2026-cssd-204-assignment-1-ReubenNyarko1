513ccf625dee5ab9436bf24c5114e54d2e70a3fe
ReubenNyarko1 <jnrsterry17@gmail.com>
2026-05-30 16:45:00 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,26 @@
 
 def main():
   pass #code goes here
+ # Prints my initials in stylized large letters
+
+    print()
+
+    print("...RAN")
+
+    print()
+
+    print("...RRRRRRRRRRRR...****...AAAAAA..AAAA...****...NNN......NNN...****")
+    print("...RRR......RRR...****...AAA......AAA...****...NNNN.....NNN...****")
+    print("...RRR......RRR...****...AAA......AAA...****...NNN..N...NNN...****")
+    print("...RRRRRRRRRRR....****...AAAAAAAAAAAA...****...NNN...N..NNN...****")
+    print("...RRR...RRR......****...AAA......AAA...****...NNN....N.NNN...****")
+    print("...RRR....RRR.....****...AAA......AAA...****...NNN.....NNNN...****")
+    print("...RRR.....RRR....****...AAA......AAA...****...NNN......NNN...****")
+    print("...RRR......RRR...****...AAA......AAA...****...NNN......NNN...****")
+    print("...RRR......RRR...****...AAA......AAA...****...NNN......NNN...****")
+    print("...RRR......RRR...****...AAA......AAA...****...NNN......NNN...****")
+
+    print()
 
 
 
